011441a5aad4d40d04a979313b68267865bc98ff
antero <antero2203@gmail.com>
2023-10-28 10:51:07 -0500

    first change

---
+++ 01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+   
+//   var myName = 'Fritz' ;
+//   latefinal myName;
+//   String var myName = 'Fritz' ;
+   const myName = 'Fritz';
+   
+   
+   print('Hola $myName');
+   print('Hola ${ myName.toUpperCase() }');
+   print('Hola ${ 1 + 1 }');
+ }
